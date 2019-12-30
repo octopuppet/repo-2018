6c3a46b9dbd7a5754374b20b0de9cb2b32c08a07
octopuppet <56733287+octopuppet@users.noreply.github.com>
2019-12-30 12:33:46 -0500

    Create error.pp

---
+++ site-modules/ex9/error.pp
@@ -0,0 +1,8 @@
+plan ex9::error (TargetSpec $targets) {
+  $results = run_command('false', $targets)
+  if $results.ok {
+    out::message("The command succeeded")
+  } else {
+    out::message("The command failed")
+  }
+}
